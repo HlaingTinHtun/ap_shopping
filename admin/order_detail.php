8f6e6bfb9eaf5f3d25e38fc0b68f1a087700b926
Hlaing Tin Htun <hlaingtinhtun@gmail.com>
2020-09-19 17:10:45 +0900

    week 6 order detail page pagination bug fix

--- admin/order_detail.php
+++ admin/order_detail.php
@@ -86,15 +86,15 @@ if ($_SESSION['role'] != 1) {
                 </table><br>
                 <nav aria-label="Page navigation example" style="float:right">
                   <ul class="pagination">
-                    <li class="page-item"><a class="page-link" href="?pageno=1">First</a></li>
+                    <li class="page-item"><a class="page-link" href="?id=<?php echo $_GET['id']?>&pageno=1">First</a></li>
                     <li class="page-item <?php if($pageno <= 1){ echo 'disabled';} ?>">
-                      <a class="page-link" href="<?php if($pageno <= 1) {echo '#';}else{ echo "?pageno=".($pageno-1);}?>">Previous</a>
+                      <a class="page-link" href="<?php if($pageno <= 1) {echo '#';}else{ echo "?id=".$_GET['id']."&pageno=".($pageno-1);}?>">Previous</a>
                     </li>
                     <li class="page-item"><a class="page-link" href="#"><?php echo $pageno; ?></a></li>
                     <li class="page-item <?php if($pageno >= $total_pages){ echo 'disabled';} ?>">
-                      <a class="page-link" href="<?php if($pageno >= $total_pages) {echo '#';}else{ echo "?pageno=".($pageno+1);}?>">Next</a>
+                      <a class="page-link" href="<?php if($pageno >= $total_pages) {echo '#';}else{ echo "?id=".$_GET['id']."&pageno=".($pageno+1);}?>">Next</a>
                     </li>
-                    <li class="page-item"><a class="page-link" href="?pageno=<?php echo $total_pages?>">Last</a></li>
+                    <li class="page-item"><a class="page-link" href="?id=<?php echo $_GET['id']?>&pageno=<?php echo $total_pages?>">Last</a></li>
                   </ul>
                 </nav>
               </div>
