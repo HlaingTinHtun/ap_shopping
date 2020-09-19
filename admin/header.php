e1e0b383cd50e66a108dd3bda2be90cd90ac7e7c
Hlaing Tin Htun <hlaingtinhtun@gmail.com>
2020-09-19 17:10:19 +0900

    week 6 search bug fix

--- admin/header.php
+++ admin/header.php
@@ -37,27 +37,31 @@ scratch. This page gets rid of all links and provides the needed markup only.
         $page = end($link_array);
     ?>
 
-  <?php if ($page != 'order_list.php') {?>
-      <form class="form-inline ml-3" method="post"
-      <?php if($page == 'index.php') :?>
-        action="index.php"
-      <?php elseif($page == 'category.php'):?>
-        action="category.php"
-      <?php elseif($page == 'user_list.php'):?>
-        action="user_list.php"
-      <?php endif;?>
-      >
-        <input name="_token" type="hidden" value="<?php echo $_SESSION['_token']; ?>">
-        <div class="input-group input-group-sm">
-          <input name="search" class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-navbar" type="submit">
-              <i class="fas fa-search"></i>
-            </button>
-          </div>
-        </div>
-      </form>
-  <?php } ?>
+    <?php if($page == 'index.php' || $page == 'category.php' || $page == 'user_list.php') {?>
+      <?php if ($page != 'order_list.php') {?>
+          <form class="form-inline ml-3" method="post"
+          <?php if($page == 'index.php') :?>
+            action="index.php"
+          <?php elseif($page == 'category.php'):?>
+            action="category.php"
+          <?php elseif($page == 'user_list.php'):?>
+            action="user_list.php"
+          <?php endif;?>
+          >
+            <input name="_token" type="hidden" value="<?php echo $_SESSION['_token']; ?>">
+            <div class="input-group input-group-sm">
+              <input name="search" class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+      <?php } ?>
+    <?php } ?>
+
+    
 
   </nav>
   <!-- /.navbar -->
